eea7e45a2c53fcb38ad86fc8fd21d3f72ea65894
dudamaciej88 <dudamaciej88@gmail.com>
2020-09-10 22:43:26 +0200

    Updates

---
+++ precache-manifest.6b226199436c6b3ef497d4c8a351aee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43275382e081d32c0f2741db2f8e76b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99c10799e25ff82812ec",
+    "url": "/static/css/main.ff2c625d.chunk.css"
+  },
+  {
+    "revision": "28c205204b2a66e60732",
+    "url": "/static/js/2.1b74b62e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b74b62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c10799e25ff82812ec",
+    "url": "/static/js/main.9690d5a9.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
