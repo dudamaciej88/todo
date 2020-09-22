555ce6a3d060618a73859bfb8d1d7434933a4cd9
dudamaciej88 <dudamaciej88@gmail.com>
2020-09-22 13:19:08 +0200

    Updates

---
+++ precache-manifest.75841739bddf31e5908b12a93854d7eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45dbea5b59a7db7d00cf3f321bec8bf0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "99c10799e25ff82812ec",
+    "url": "/todo/static/css/main.ff2c625d.chunk.css"
+  },
+  {
+    "revision": "28c205204b2a66e60732",
+    "url": "/todo/static/js/2.1b74b62e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.1b74b62e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c10799e25ff82812ec",
+    "url": "/todo/static/js/main.9690d5a9.chunk.js"
+  },
+  {
+    "revision": "85470c4886edd07fcd1e",
+    "url": "/todo/static/js/runtime-main.3e97ef50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/breaking-bad-app/precache-manifest.ed6b5e00e1d62f22f6e1b42ee326f0b6.js"
+  "/todo/precache-manifest.75841739bddf31e5908b12a93854d7eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/breaking-bad-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
